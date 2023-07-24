f7edb8f5b52fed74085190287e09cd029beb8e66
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-07-24 05:13:16 +0300

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Flask web app
+Listens on 0.0.0.0, port 5000.
+Routes:
+ /: Prints 'Hello HBNB!'.
+ hbnb: Prints 'HBNB'.
+c/<text>: Displays 'C' followed by the value of <text>.
+print python
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Prints 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Prints 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Prints 'C' followed by text"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' and text
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
